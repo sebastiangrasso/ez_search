deef72fb98a0aeed3755063d45ee3a110cc4c0a0
sebastiangrasso <33736948+sebastiangrasso@users.noreply.github.com>
2020-11-24 12:51:11 -0500

    Weather Page needs geolocation + search

---
+++ src/WeekContainer.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import weatherConfig from './apiKeys';
+import DayCard from './DayCard'
+
+class WeekContainer extends React.Component {
+  state = {
+    fullData: [],
+    dailyData: []
+  }
+
+  componentDidMount = () => {
+    const weatherURL = 'http://api.openweathermap.org/data/2.5/forecast?zip=11102&units=imperial&APPID=4e2f926c98f19cc625c4f96afef5a53c'
+
+    console.log(weatherURL)
+
+
+    fetch(weatherURL)
+    .then(res => res.json())
+    .then(data => {
+      const dailyData = data.list.filter(reading => reading.dt_txt.includes("18:00:00"))
+      this.setState({
+        fullData: data.list,
+        dailyData: dailyData
+      }, () => console.log(this.state))
+    })
+  }
+
+  formatDayCards = () => {
+    return this.state.dailyData.map((reading, index) => <DayCard reading={reading} key={index} />)
+  }
+
+  render() {
+    return (
+      <div className="container">
+      <h1 className="display-1 jumbotron">Weather</h1>
+      <h5 className="display-5 text-muted">New York, US</h5>
+        <div className="row justify-content-center">
+
+          {this.formatDayCards()}
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default WeekContainer;
--- src/index.js
+++ src/index.js
@@ -1,67 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
 import './index.css';
+import WeekContainer from './WeekContainer';
 
-class Square extends React.Component {
+class App extends Component {
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
-}
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
-  }
-
-  render() {
-    const status = 'Next player: X';
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+      <div className="App">
+        <WeekContainer />
       </div>
     );
   }
 }
 
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
+//export default App;
 // ========================================
 
 ReactDOM.render(
-  <Game />,
+  <App />,
   document.getElementById('root')
 );
