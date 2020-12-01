8aee4157c9c16e5fe9ec96dcd198cca4276af113
Kyle Traverse <45571914+thekyletraverse@users.noreply.github.com>
2020-11-30 22:38:27 -0500

    Create Directions.js

---
+++ src/components/Directions.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
+
+const divStyle = {
+  marginLeft: "10px",
+  marginRight: "10px",
+  marginTop: "10px",
+  marginBottom: "10px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
+class Directions extends Component {
+  render() {
+    return (
+      <Container>
+        <Row className="justify-content-md-center" style={{ marginTop: 60 }}>
+          <Col style={divStyle}>
+            <h1>Looking for directions?</h1>
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center" style={{ marginTop: 60 }}>
+          <Col style={divStyle}>
+            <Form>
+              <label>
+                Street Address:
+                <input type="text" name="name" />
+              </label>
+            </Form>
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center" style={{ marginTop: 30 }}>
+          <Col style={divStyle}>
+            <Form>
+              <label>
+                City:
+                <input type="text" name="name" />
+              </label>
+            </Form>
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center" style={{ marginTop: 30 }}>
+          <Col style={divStyle}>
+            <Form>
+              <label>
+                State:
+                <input type="text" name="name" />
+              </label>
+            </Form>
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center" style={{ marginTop: 30 }}>
+          <Col style={divStyle}>
+            <Button>Search</Button>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Directions;
