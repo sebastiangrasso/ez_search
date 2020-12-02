f5555332ad4c8d8fe5f3cd82cd6b70cd7c94eba2
sebastiangrasso <33736948+sebastiangrasso@users.noreply.github.com>
2020-12-02 12:28:39 -0500

    search card format. Transactional done, must add other search methods

--- src/SearchCard.js
+++ src/SearchCard.js
@@ -1,19 +1,50 @@
 import React from 'react';
+import ReactTooltip from 'react-tooltip';
+import './SearchCard.css'
+
 //import {Card} from 'bootstrap'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+const no_image = "https://thumbs.dreamstime.com/b/no-image-available-icon-vector-illustration-flat-design-140633878.jpg"
 
 const SearchCard = ({ reading }) => {
   let title = reading.title
-  let desc = reading.snippet
+  let snippet = reading.snippet
   let link = reading.link
-
+  let pagemap = reading.pagemap
+  let prev = no_image
+  console.log(title)
+  if (pagemap !== undefined){
+    if (pagemap.cse_image!== undefined){
+      prev = pagemap.cse_image[0].src;
+      console.log(pagemap)
+    }
+  }
   return (
     <div class="container-fluid">
-      <div class="row">
-          <Route path='/res' component={() => window.location = {link}}/>
-          <a href="/res">{title}</a>
-          <p>{desc}</p>
-      </div>
+        <div class="row">
+            <div class="col-12 mt-3">
+                <div class="card">
+                    <div class="card-horizontal">
+                        <div class="img-square-wrapper">
+                        <a target="_blank" href={decodeURI(link)}>
+                          <img className="preview" src={prev} width={350} height={300}/>
+                        </a>
+                        </div>
+                        <div class="card-body">
+                        <h4 className="title">
+                          <a className="ext-link" target="_blank" href={decodeURI(link)}>
+                            {title}
+                          </a>
+                        </h4>
+                            <p class="card-text">{snippet}}</p>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
